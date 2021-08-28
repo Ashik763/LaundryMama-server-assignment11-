980689e99153a56134a539e7bc47b99fd7d03b40
Ashik Ghosh <63238405+Ashik763@users.noreply.github.com>
2021-08-28 21:39:30 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
+// const corsOptions ={ origin:'*', credentials:true} //access-control-allow-credentials:true optionSuccessStatus:200, }
+const fs = require("fs-extra");
 const fileUpload = require('express-fileupload');
 const MongoClient = require("mongodb").MongoClient;
 const { ObjectId } = require("mongodb");
@@ -10,94 +12,218 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
+// app.use(cors(corsOptions))
 app.use(express.static('pictures'));
 app.use(fileUpload());
 
 const port = 5000;
 app.get("/", (req, res) => {
-  res.send("hello world2");
+  res.send("hello world52");
 });
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true 
 });
 client.connect((err) => {
-  // console.log(err);
+  // //console.log(err);
   const reviewCollection = client.db("MamaLaundry").collection("reviews");
+  const serviceCollection = client.db("MamaLaundry").collection("services");
+  const orderCollection = client.db("MamaLaundry").collection("orderCollection");
+  const admins = client.db("MamaLaundry").collection("admins");
+
+
 
   app.get("/showReview", (req, res) => {
-    reviewCollection.find({}).toArray((err, documents) => {
+    reviewCollection.find({})
+    .toArray((err, documents) => {
       res.send(documents);
-      // console.log(documents);
+      
     });
   });
 
   app.post("/addReview", (req, res) => {
-    const review = req.body;
-    // console.log(review);
-    reviewCollection.insertOne(review).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
+   
+    const file = req.files.file;
+    const name = req.body.name;
+    const description = req.body.description;
+    //console.log(name,description,file);
+
+    const filePath = `${__dirname}/pictures/${file.name}`;
+    file.mv(`${__dirname}/pictures/${file.name}`,err => {
+      if(err){
+        //console.log(err);
+        return res.status(500).send({msg:'failed to upload Image'})
+      }
+      
+      var newImg = fs.readFileSync(filePath);
+      const encImg = newImg.toString('base64');
+  
+      var image = {
+          contentType: file.mimetype,
+          size: file.size,
+          img: Buffer.from(encImg, 'base64')
+      };
+  
+      reviewCollection.insertOne({name,description,image})
+      .then((result) => {
+        fs.remove(filePath,error => { 
+          if(error){
+            
+            //console.log(error)
+            return res.status(500).send({msg:'failed to upload Image'})
+          }
+          res.send(result.insertedCount > 0);
+         
+         })
+       
+      });
+
+    })
+
   });
 
-  const serviceCollection = client.db("MamaLaundry").collection("services");
-  app.post("/addService", (req, res) => {
-    const service = req.body;
-    console.log(service);
-    serviceCollection.insertOne(service).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-    // .catch(err => {console.log(err)})
-  });
+  app.post('/addService',(req, res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    const description = req.body.description;
+    const price = req.body.price;
+    //console.log(name,description,file,price);
+
+    const filePath = `${__dirname}/pictures/${file.name}`;
+    file.mv(`${__dirname}/pictures/${file.name}`,err => {
+      if(err){
+        //console.log(err);
+        return res.status(500).send({msg:'failed to upload Image'})
+      }
+      
+      var newImg = fs.readFileSync(filePath);
+      const encImg = newImg.toString('base64');
+  
+      var image = {
+          contentType: file.mimetype,
+          size: file.size,
+          img: Buffer.from(encImg, 'base64')
+      };
+  
+      serviceCollection.insertOne({name,description,price,image})
+      .then((result) => {
+        fs.remove(filePath,error => { 
+          if(error){
+            
+            //console.log(error)
+            return res.status(500).send({msg:'failed to upload Image'})
+          }
+          res.send(result.insertedCount > 0);
+         
+         })
+       
+      });
+
+    })
+
+
+
+  })
+  
+  
+  
+  
   app.get("/showService", (req, res) => {
     serviceCollection.find({}).toArray((err, documents) => {
       res.send(documents);
-      // console.log(documents);
+      // //console.log(documents);
     });
   });
 
-    app.post("/OrderService/:id", (req, res) => {
-    const OrderService = req.body;
-    
-    serviceCollection.insertOne(OrderService).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-    // .catch(err => {console.log(err)})
-  });
 
 
-  app.get("/OrderService/:id", (req, res) => {
+
+   app.get("/OrderService/:id", (req, res) => {
     const id= req.params.id;
       serviceCollection.find({_id:ObjectId(id)}).toArray((err, documents) => {
       res.send(documents);
-      // console.log(documents);
+      // //console.log(documents);
     });
   });
 
   
 
-  const orderCollection = client.db("MamaLaundry").collection("orderCollection");
-  app.post("/userInfo/:phone", (req, res) => {
+  app.post("/userInfo", (req, res) => {
     const order = req.body;
-    // console.log(order);
+  
     
     orderCollection.insertOne(order).then((result) => {
       res.send(result.insertedCount > 0);
     });
-    // .catch(err => {console.log(err)})
+    // .catch(err => {//console.log(err)})
   });
 
-  app.get("/userInfo/:phone", (req, res) => {
-    const phone = req.params.phone;
-    orderCollection.find({phone:phone}).toArray((err, documents) => {
+  app.get("/userInfo/:email", (req, res) => {
+    const email = req.params.email;
+    //console.log(email);
+    orderCollection.find({email:email}).toArray((err, documents) => {
       res.send(documents);
-      // console.log(documents);
+      // //console.log(documents);
     });
   });
 
+  app.get("/checkAdmin/:email", (req, res) => {
+    const email = req.params.email;
+    // //console.log(email);
+    admins.find({email:email}).toArray((err, documents) => {
+      res.send(documents.length > 0);
+      // //console.log(documents);
+    });
+  });
 
+  app.get('/allOrders', (req, res) => {
+    orderCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+      // //console.log(documents);
+    });
+
+
+
+  })
+
+  app.patch("/orderWithState", (req, res) => {
+   
+    const allOrders = req.body;
+    // console.log(state);
+    orderCollection.updateOne({_id:ObjectId(allOrders._id)},
+  {
+    $set:{state:allOrders.state}
+  })
+  .then(result => {
+    // console.log(result.connection);
+  })
+  })
+  app.delete("/deleteOrder/:id", (req, res) => {
+   
+    id = req.params.id;
+    console.log(id);
+    orderCollection.deleteOne({_id:ObjectId(id)})
+
+  .then(result => {
+    console.log('successfully deleted');
+  })
+
+
+
+  })
+
+  app.post("/makeAdmin", (req, res) => {
+    const admin = req.body;
+    console.log(admin);
+  
+    
+    admins.insertOne(admin).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+    // .catch(err => {//console.log(err)})
+  });
 
   
 
